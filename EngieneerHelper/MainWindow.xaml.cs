7b74ed3cf421931b63a6f5e767c3c5352c65e45d
deL <Salexey.61rus@gmail.com>
2018-07-26 01:45:04 +0300

    UI Create

--- EngieneerHelper/MainWindow.xaml.cs
+++ EngieneerHelper/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace EngieneerHelper
 {
@@ -23,6 +24,11 @@ namespace EngieneerHelper
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0x0);
         }
     }
 }
